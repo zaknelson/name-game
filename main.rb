5185f5905103a431aa2a61b0823860881911e3da
Zak Nelson <zak.nelson@gmail.com>
2016-12-27 12:54:07 -0800

    Add names api.

--- main.rb
+++ main.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'elo'
+require 'json'
 
   
 players = Hash.new
 
-File.open("my/file/path", "r") do |f|
+File.open(File.join(settings.public_folder, 'names.csv'), "r") do |f|
   f.each_line do |line|
     values = line.split(",")
-    players[values[0]] = {:name => values[0], :elo_player => Elo::Player.new(:rating => values[1]) };
+    players[values[0]] = {:name => values[0], :elo_player => Elo::Player.new(:rating => values[1].to_i) };
   end
 end 
 
@@ -18,6 +19,7 @@ end
 get '/names' do
   content_type :json 
   sorted_players = players.values;
+  result = []
   sorted_players.sort! do |x, y| 
     if x[:elo_player].rating == y[:elo_player].rating
       x[:elo_player].rating <=> y[:elo_player].rating 
@@ -25,7 +27,10 @@ get '/names' do
       x[:name] <=> y[:name]
     end
   end
-  sorted_players.to_json
+  sorted_players.each do |player|
+    result.push({:name => player[:name], :rating => player[:elo_player].rating})
+  end
+  result.to_json
 end
 
 get '/' do
