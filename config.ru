1afe8c706bf78ef11fdfb64ecdad8e9eec26c7d4
Zak Nelson <zak.nelson@gmail.com>
2016-12-27 12:06:36 -0800

    Add ELO.

--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
-require './main'
-run Sinatra::Application
+require File.expand_path('main', File.dirname(__FILE__))
+
+run MyApp
--- main.rb
+++ main.rb
@@ -1,5 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'elo'
 
-get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+class MyServer < Sinatra::Base
+
+  def self.run!
+    @player = Elo::Player.new(:rating => 1500)
+    super
+  end
+
+  get '/' do
+    send_file File.join(settings.public_folder, 'index.html')
+  end
+
+  get '/test' do
+    @player.rating
+  end
 end
