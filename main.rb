899cd622e41ccc3cf90de758d82ee96c9199d89c
Zak Nelson <zak.nelson@gmail.com>
2016-12-27 12:10:52 -0800

    Cleanups.

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
 require 'elo'
 
-    player = Elo::Player.new(:rating => 1500)
-  get '/' do
-    send_file File.join(settings.public_folder, 'index.html')
-  end
+  
+player = Elo::Player.new(:rating => 1500)
 
-  get '/test' do
-    player.rating
-  end
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+get '/test' do
+  "hello"
+end
+
+get '/test2' do
+  player.rating
+end
