fdaf5eac286b1aa93a64e2f5379549aa64c8df0f
Zak Nelson <zak.nelson@gmail.com>
2016-12-27 12:35:47 -0800

    Return more data.

--- main.rb
+++ main.rb
@@ -2,16 +2,32 @@ require 'sinatra'
 require 'elo'
 
   
-player = Elo::Player.new(:rating => 1500)
+players = Hash.new
+
+File.open("my/file/path", "r") do |f|
+  f.each_line do |line|
+    values = line.split(",")
+    players[values[0]] = {:name => values[0], :elo_player => Elo::Player.new(:rating => values[1]) };
+  end
+end 
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
-get '/test' do
-  "hello"
+get '/names' do
+  content_type :json 
+  sorted_players = players.values;
+  sorted_players.sort! do |x, y| 
+    if x[:elo_player].rating == y[:elo_player].rating
+      x[:elo_player].rating <=> y[:elo_player].rating 
+    else
+      x[:name] <=> y[:name]
+    end
+  end
+  sorted_players.to_json
 end
 
-get '/test2' do
+get '/' do
   player.rating.to_s
 end
